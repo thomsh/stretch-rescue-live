7070b604958e4a78baee17d034d4ce6cc995fbd9
Steven Monai <steve-gh@monai.ca>
2018-04-21 11:34:48 -0700

    Rename authorized_keys to authorized_keys.d; tweak make.sh
    Changes committed:
    	renamed:    authorized_keys/readme.txt -> authorized_keys.d/readme.txt
    	modified:   make.sh

--- make.sh
+++ make.sh
@@ -9,7 +9,26 @@ fn="stretch-rescue-live"
 
 me=$(readlink -e "$0")
 wd=$(dirname "${me}")
+wb=$(basename "${me}")
 cd "${wd}"
+
+##
+## Check that I'm running as root
+##
+
+if [ $(id -u) -ne 0 ]; then
+
+	echo
+	echo "${wb} must be run as root (or with sudo)."
+	echo
+	exit 1
+fi
+
+##
+## Clean up ISOs from previous build run.
+##
+
+rm -f *.iso
 
 ##
 ## Load the desired ssh pub-keys into the live chroot.
@@ -21,7 +40,7 @@ mkdir -p	"${root_home}/.ssh/"
 : >		"${root_home}/.ssh/authorized_keys"
 chmod -R go-rwx	"${root_home}"
 
-find authorized_keys -iname '*.pub' -print | sort |
+find authorized_keys.d -iname '*.pub' -print | sort |
 while read pk ; do
 	cat "${pk}" >> "${root_home}/.ssh/authorized_keys"
 	echo "Added '${pk}'"
@@ -49,10 +68,14 @@ my_iso="${fn}-$(date +%Y%m%d.%H%M).iso"
 if [ -e "${org_iso}" ]; then
 	mv "$org_iso" "$my_iso"
 	ln -s "$my_iso" "$org_iso"
+	echo
 	echo "Generated '$my_iso'"
+	echo
 	exit 0
 else
+	echo
 	echo "Failed to create the Debian Live ISO."
+	echo
 	exit 1
 fi
 
