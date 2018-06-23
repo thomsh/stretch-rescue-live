aebf574bb67446fef516e6b023626c8bf36bc670
Steven Monai <steve-gh@monai.ca>
2018-06-22 19:43:55 -0700

    Generate grub config snippet that can boot ISO from a loopback mount.
    Changes committed:
    	modified:   make.sh

--- make.sh
+++ make.sh
@@ -66,17 +66,65 @@ lb build
 set +x
 
 ##
-## Put a timestamp in the generated ISO's filename.
+## Function definitions
+##
+
+generate_m4_macro() {
+	local f="$1"
+	local v="$2"
+	local m="${f//-/_}_LATEST"
+	m="${m^^}"
+	echo "define(\`${m}',\`${v}')dnl"
+}
+
+col2_pad_len() {
+	local colstr="$1"
+	local maxcol="stretch-rescue-live"
+	local maxlen=${#maxcol}
+	local padlen=$[ $maxlen - ${#colstr} ]
+	if [ $padlen -ge 0 ]; then
+		echo $padlen
+	else
+		echo 0
+	fi
+}
+
+generate_grub_menuentry() {
+	local f="$1"
+	local v="$2"
+	printf "menuentry ' Debian Live amd64 :: ${f}%*s :: ${v} ' {\n" $(col2_pad_len "${f}")
+	cat <<EOF
+	iso_path="/boot/isos/${f}-${v}.iso"
+	export iso_path
+	kernelopts=" live-media=removable-usb "
+	export kernelopts
+	loopback loop "\$iso_path"
+	set root=(loop)
+	configfile "/boot/grub/loopback.cfg"
+}
+EOF
+}
+
+##
+## Put a version (timestamp) in the generated ISO's filename.
 ##
 
 org_iso="live-image-amd64.hybrid.iso"
 version="$(date +%Y%m%d.%H%M)"
 my_iso="${fn}-${version}.iso"
-m4_name="${fn//-/_}_LATEST" ; m4_name="${m4_name^^}"
+
+##
+## Generate some useful artifacts related to the new ISO:
+## - an m4 macro containing the version string (timestamp)
+## - a snippet of grub config that can boot the ISO from a loopback mount
+##
+
 m4_file="${fn}.m4"
+grub_file="${fn}_grub-loop-boot.cfg"
 
 if [ -e "${org_iso}" ]; then
-	echo "define(\`${m4_name}',\`${version}')dnl" > "${m4_file}"
+	generate_m4_macro "${fn}" "${version}" > "${m4_file}"
+	generate_grub_menuentry "${fn}" "${version}" > "${grub_file}"
 	mv "$org_iso" "$my_iso"
 	ln -s "$my_iso" "$org_iso"
 	echo
